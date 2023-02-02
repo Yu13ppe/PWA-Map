ee6ff7aaf05aef6a48dc70e5dc633781fae30325
Jose Portillo <joseportillo2002.jdpf@gmail.com>
2023-02-02 01:54:36 -0400

    add-reactstrap

--- src/Pages/Lines.js
+++ src/Pages/Lines.js
@@ -1,6 +1,7 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
+import {Container, Row, Col, Button, Card, CardBody, CardHeader, CardFooter, CardText, CardTitle } from 'reactstrap';
 import { lineList } from '../Components/LineList';
-
+import { FaRegHeart, FaHeart } from "react-icons/fa";
 
 function Lines() {
     const [like, setLike] = useState(false);
@@ -9,9 +10,39 @@ function Lines() {
         setLike(!like);
     }
 
-  return (
-    <div>Lines</div>
-  )
+    return (
+        <Container fluid className="content">
+            <Row>
+                {
+                    lineList.map(line => (
+                        <Col key={line.id}>
+                            <Card className="card">
+                                <CardHeader className="card-head">
+                                    id {line.id}
+                                </CardHeader>
+                                <CardBody className="card-body">
+                                    <CardTitle className="card-tittle">
+                                        {line.name}
+                                    </CardTitle>
+                                </CardBody>
+                                <CardFooter className="card-footer">
+                                    <Button className="btn" type="button">
+                                        {
+                                            like ? (
+                                                <FaHeart className="icon" onClick={handleLike} />
+                                            ) : (
+                                                <FaRegHeart className="icon" onClick={handleLike} />
+                                            )
+                                        }
+                                    </Button>
+                                </CardFooter>
+                            </Card>
+                        </Col>
+                    ))
+                }
+            </Row>
+        </Container>
+    )
 }
 
-export {Lines}
+export { Lines }
