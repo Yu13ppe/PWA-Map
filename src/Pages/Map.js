3aab27a43067867977d4c691f841697d61af20a5
Jose Portillo <joseportillo2002.jdpf@gmail.com>
2023-03-06 16:03:54 -0400

    Example one route

--- src/Pages/Map.js
+++ src/Pages/Map.js
@@ -1,11 +1,24 @@
 import React from 'react'
-import { MapContainer, TileLayer} from 'react-leaflet'
+import { MapContainer, TileLayer, Polyline} from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 import { LocationMarker } from '../Components/LocationMarker'
 
 function MapView() {
 
   const position = [10.693, -71.634]
+
+  const multiPolyline = [
+    [
+      [10.668782, -71.623452],
+      [10.671903, -71.626156],
+      [10.673611, -71.626714],
+      [10.677050, -71.627663],
+      [10.678674, -71.628583],
+      [10.685021, -71.632972],
+      [10.711477, -71.640439]
+    ],
+  ]
+  const limeOptions = { color: 'lime' }
 
   return (
     <div className='MapView'>
@@ -15,6 +28,7 @@ function MapView() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
         <LocationMarker />
+        <Polyline pathOptions={limeOptions} positions={multiPolyline} />
       </MapContainer>
     </div>
   )
