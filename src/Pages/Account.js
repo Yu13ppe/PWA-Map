c2b5b9892b1edf504e1c78ba8c7afaef3c7673e6
Jose Portillo <joseportillo2002.jdpf@gmail.com>
2023-03-06 10:52:07 -0400

    Login scss

--- src/Pages/Account.js
+++ src/Pages/Account.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from "react-router-dom";
-
+import { Label, Input } from 'reactstrap';
+//correoInicioSesion
 function Account() {
 
   return (
@@ -8,16 +9,38 @@ function Account() {
       <div className='containerInicioSesion'>
         <h1 className='titulo'>
           Inicio de Sesión
-          <hr className='rayaTitulo' />
+          <div className='rayaTitulo' />
         </h1>
         <div className='containerInterno'>
-          <h2 className='correoInicioSesion'>Correo Electronico</h2>
-          <input className='containerCorreo' placeholder='Introduzca su correo' />
-          <h2 className='passwordInicioSesion'>Password</h2>
-          <input className='containerPassword' placeholder='Introduzca su contraseña' />
-          <h3 className='olvidarContraseña'><Link to="/Recover">¿Olvidaste la contraseña?</Link></h3>
-          <button className='botonInicio'>Iniciar Sesión</button>
-          <Link to="/Register"><button className='botonRegistro'>Registrar</button></Link>
+          <div className='Form'>
+            <Label className='correoInicioSesion' for="exampleEmail">
+              Email
+            </Label>
+            <Input 
+            className='containerCorreo' 
+            type="email" 
+            name="email" 
+            id="exampleEmail" 
+            placeholder="Introduzca su correo" 
+            />
+            <Label className='passwordInicioSesion' for="examplePassword">
+              Password
+            </Label>
+            <Input
+              className='containerPassword'
+              id="examplePassword"
+              name="password"
+              placeholder="password placeholder"
+              type="password"
+            />
+            <h3 className='olvidarContraseña'>
+              <Link to="/Recover">¿Olvidaste la contraseña?</Link>
+            </h3>
+            <div className='LoginButtons'>
+              <Link to="/Perfil"><button className='botonInicio btnLogin'>Iniciar Sesión</button></Link>
+              <Link to="/Register"><button className='botonRegistro btnLogin'>Registrar</button></Link>
+            </div>
+          </div>
         </div>
       </div>
     </div>
