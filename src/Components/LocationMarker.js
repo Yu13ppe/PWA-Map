781ce4c3aab267c595cce5ce27f2f0bc5532040a
Jose Portillo <111423928+Yu13ppe@users.noreply.github.com>
2024-02-25 17:59:28 -0400

    Add files via upload

--- src/Components/LocationMarker.js
+++ src/Components/LocationMarker.js
@@ -1,27 +1,25 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { Marker, Popup, useMapEvents } from 'react-leaflet'
 import { IconLocation3 } from "../Components/IconLocationHere";
 
 function LocationMarker() {
-    const [position, setPosition] = useState(null)
-    const map = useMapEvents({
-      click() {
-          map.locate();
-      },
-      locationfound(e) {
-        setInterval(() => {
-          setPosition(e.latlng)
-          // console.log(e.latlng)
-      }, 20000);
-        map.flyTo(e.latlng, map.getZoom())
-      },
-    })
-  
-    return position === null ? null : (
-      <Marker position={position} icon={IconLocation3}>
-        <Popup>You are here</Popup>
-      </Marker>
-    )
-  }
+  const [position, setPosition] = useState(null)
+  const map = useMapEvents({
+    click() {
+      map.locate();
+    },
+    locationfound(e) {
+      setPosition(e.latlng)
+      // console.log(e.latlng)
+      map.flyTo(e.latlng, map.getZoom())
+    },
+  })
 
-export {LocationMarker}
+  return position === null ? null : (
+    <Marker position={position} icon={IconLocation3}>
+      <Popup>You are here</Popup>
+    </Marker>
+  )
+}
+
+export { LocationMarker }
