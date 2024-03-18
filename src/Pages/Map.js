6b247ceeba7f31ed554fa7a4643e69e65e292180
Jose Portillo <111423928+Yu13ppe@users.noreply.github.com>
2024-03-18 12:56:11 -0400

    Update Map.js

--- src/Pages/Map.js
+++ src/Pages/Map.js
@@ -64,7 +64,7 @@ function MapView() {
   useEffect(() => {
     const intervalId = setInterval(() => {
       setRefreshKey(prevKey => prevKey + 1);
-    }, 2000); // 5000 ms = 5 s
+    }, 5000); // 5000 ms = 5 s
 
     // Limpiar el intervalo cuando el componente se desmonte
     return () => clearInterval(intervalId);
